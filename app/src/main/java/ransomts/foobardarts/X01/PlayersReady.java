cb07db086259be00fe39ad22035629a8988047ee
ransomts <ransomts@appstate.edu>
2017-05-16 20:11:57 -0400

    New object to represent the players waiting for a game

--- app/src/main/java/ransomts/foobardarts/X01/PlayersReady.java
+++ app/src/main/java/ransomts/foobardarts/X01/PlayersReady.java
@@ -1,29 +1,90 @@
 package ransomts.foobardarts.X01;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * Created by tim on 4/17/17.
+ * Small class to help determine when all networked players are ready to begin,
+ * created to be compatible with the firebase database and able to be passed around in Android
  */
 
-class PlayersReady {
-    Map<String, Boolean> playersReady;
+public class PlayersReady implements Parcelable {
+    HashMap<String, Boolean> playersReady;
 
-    public PlayersReady() {    }
+    public PlayersReady() {
+        playersReady = new HashMap<>();
+    }
 
-    public PlayersReady(Map<String, Boolean> playersReady) {
+    public PlayersReady(HashMap<String, Boolean> playersReady) {
         setPlayersReady(playersReady);
     }
 
-    public void setPlayersReady(Map<String,Boolean> playersReady) {
+    public void setPlayersReady(HashMap<String, Boolean> playersReady) {
         this.playersReady = playersReady;
     }
 
-    public Map<String, Boolean> getPlayersReady() {
+    public boolean allPlayersReady() {
+
+        for (String key : playersReady.keySet()) {
+            if (!playersReady.get(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void updatePlayerStatus(String player, boolean status) {
+        playersReady.put(player, status);
+    }
+
+    public HashMap<String, Boolean> getPlayersReady() {
         return playersReady;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.playersReady.size());
+        for (Map.Entry<String, Boolean> entry : this.playersReady.entrySet()) {
+            dest.writeString(entry.getKey());
+            dest.writeValue(entry.getValue());
+        }
+    }
+
+    public void addPlayer(String playerName) {
+        // All players start out not ready
+        playersReady.put(playerName, false);
+    }
+
+    protected PlayersReady(Parcel in) {
+        int playersReadySize = in.readInt();
+        this.playersReady = new HashMap<String, Boolean>(playersReadySize);
+        for (int i = 0; i < playersReadySize; i++) {
+            String key = in.readString();
+            Boolean value = (Boolean) in.readValue(Boolean.class.getClassLoader());
+            this.playersReady.put(key, value);
+        }
+    }
+
+    public static final Parcelable.Creator<PlayersReady> CREATOR = new Parcelable.Creator<PlayersReady>() {
+        @Override
+        public PlayersReady createFromParcel(Parcel source) {
+            return new PlayersReady(source);
+        }
+
+        @Override
+        public PlayersReady[] newArray(int size) {
+            return new PlayersReady[size];
+        }
+    };
 }
