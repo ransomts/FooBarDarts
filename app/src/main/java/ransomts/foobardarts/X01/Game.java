4365206fb3f1ebe141360310e45cc23414b52a5c
ransomts <ransomts@appstate.edu>
2017-05-16 20:11:06 -0400

    Abstracted out misplaced game logic
    Implemented Parcelable interface for the game and made sure the firbase database took the X01 game class as an object

--- app/src/main/java/ransomts/foobardarts/X01/Game.java
+++ app/src/main/java/ransomts/foobardarts/X01/Game.java
@@ -1,42 +1,79 @@
 package ransomts.foobardarts.X01;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
- * Model of a Game entry in the firbase database
+ * Model of a Game entry in the firebase database
  */
 
-class Game {
-    private String gameId;
+abstract class Game implements Parcelable {
+
+    private int shotsPerTurn;
     private Date startTime;
+    private Date endTime;
+    private String gameId;
     private String winner;
-    private ArrayList<String> players;
+    private Turn mostRecentTurn;
+    // Having this be an abstracted class adds a layer to the database playersReady entry
+    // Maybe that's not so good? It looks fine for now
     private PlayersReady playersReady;
     private ArrayList<Turn> turns;
-    private Turn mostRecentTurn;
+    private HashMap<String, Integer> currentScores;
+
+    private DatabaseReference gameRef;
 
     public Game() {}
 
-    public Game(String gameId, Date startTime, String winner, ArrayList<String> players,
-                PlayersReady playersReady, ArrayList<Turn> turns, Turn mostRecentTurn) {
-        setGameId(gameId);
+    // Constructor specifically for the X01_game class
+    public Game(int shotsPerTurn,
+                String gameId,
+                PlayersReady playersReady) {
+        this(shotsPerTurn, null, null, gameId, null, null, playersReady, null, null, null);
+    }
+
+    // Do we already know everything?
+    public Game(int shotsPerTurn,
+             Date startTime,
+             Date endTime,
+             String gameId,
+             String winner,
+             Turn mostRecentTurn,
+             PlayersReady playersReady,
+             ArrayList<String> players,
+             ArrayList<Turn> turns,
+             HashMap<String, Integer> currentScores) {
+
+        setShotsPerTurn(shotsPerTurn);
         setStartTime(startTime);
+        setEndTime(endTime);
+        setGameId(gameId);
         setWinner(winner);
-        setPlayers(players);
-        setPlayersReady(playersReady);
-        setTurns(turns);
         setMostRecentTurn(mostRecentTurn);
+        setPlayersReady(playersReady);
+        //setPlayers(players);
+        setTurns(turns);
+        setCurrentScores(currentScores);
+
+        setGameRef();
+
+        setInitialScores();
     }
 
     // game identifier for the games node in the database
     public void setGameId(String gameId) {
+
         this.gameId = gameId;
+        setGameRef();
     }
-    public String getGameId() {
-        return gameId;
-    }
+    public String getGameId() { return gameId; }
 
     // Time that the game began play
     public void setStartTime(Date startTime) {
@@ -46,20 +83,16 @@ class Game {
         return startTime;
     }
 
+    // Time that the winner was decided
+    public void setEndTime(Date endTime) { this.endTime = endTime; }
+    public Date getEndTime() { return endTime; }
+
     // User id of the winner of the game
     public void setWinner(String winner) {
         this.winner = winner;
     }
     public String getWinner() {
         return winner;
-    }
-
-    // ArrayList of the players ids who are involved in a game
-    public void setPlayers(ArrayList<String> players) {
-        this.players = players;
-    }
-    public ArrayList<String> getPlayers() {
-        return players;
     }
 
     // Used when setting up the game in the notStarted node in the database
@@ -85,4 +118,72 @@ class Game {
     public Turn getMostRecentTurn() {
         return mostRecentTurn;
     }
+
+
+    private void setGameRef() {
+        gameRef = FirebaseDatabase.getInstance().getReference().child("games").child(getGameId());
+    }
+
+    public HashMap<String, Integer> getCurrentScores() {
+        return currentScores;
+    }
+    public void setCurrentScores(HashMap<String, Integer> currentScores) { this.currentScores = currentScores; }
+
+    public int getShotsPerTurn() {
+        return shotsPerTurn;
+    }
+    public void setShotsPerTurn(int shotsPerTurn) {
+        this.shotsPerTurn = shotsPerTurn;
+    }
+
+    // should return the key to the winner entry in the currentScores hashMap, or null if not won yet
+    abstract String winConditionMet();
+    abstract Integer calculateNewScore(Turn turn);
+    abstract void setInitialScores();
+
+    public boolean updateScore(Turn turn) {
+        // put returns a null if there was no entry with that key previously, returns the previous value if the key existed
+        return currentScores.put(turn.getPlayerId(), calculateNewScore(turn)) != null;
+    }
+
+    // returns a string of the winners ID when a winner is decided
+    public String addTurn(Turn turn) {
+        updateScore(turn);
+        return winConditionMet();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.shotsPerTurn);
+        dest.writeLong(this.startTime != null ? this.startTime.getTime() : -1);
+        dest.writeLong(this.endTime != null ? this.endTime.getTime() : -1);
+        dest.writeString(this.gameId);
+        dest.writeString(this.winner);
+        dest.writeParcelable(this.mostRecentTurn, flags);
+        dest.writeParcelable(this.playersReady, flags);
+        //dest.writeStringList(this.players);
+        dest.writeTypedList(this.turns);
+        dest.writeSerializable(this.currentScores);
+    }
+
+    protected Game(Parcel in) {
+        this.shotsPerTurn = in.readInt();
+        long tmpStartTime = in.readLong();
+        this.startTime = tmpStartTime == -1 ? null : new Date(tmpStartTime);
+        long tmpEndTime = in.readLong();
+        this.endTime = tmpEndTime == -1 ? null : new Date(tmpEndTime);
+        this.gameId = in.readString();
+        this.winner = in.readString();
+        this.mostRecentTurn = in.readParcelable(Turn.class.getClassLoader());
+        this.playersReady = in.readParcelable(PlayersReady.class.getClassLoader());
+        //this.players = in.createStringArrayList();
+        this.turns = in.createTypedArrayList(Turn.CREATOR);
+        this.currentScores = (HashMap<String, Integer>) in.readSerializable();
+    }
+
 }
--- app/src/main/java/ransomts/foobardarts/X01/X01_game.java
+++ app/src/main/java/ransomts/foobardarts/X01/X01_game.java
@@ -6,214 +6,96 @@ package ransomts.foobardarts.X01;
   Java class to model a game of 501 or 301
  */
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.os.Parcel;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Set;
 
-class X01_game extends Game{
+class X01_game extends Game {
 
-    private DatabaseReference game_ref;
-    private String game_id;
-    private Date start_time;
-    private String[] players;
-    private int playerTurnIndex;
+    // only data that is specific to a 501 game
     private int scoreGoal;
-    private boolean double_in;
-    private boolean double_out;
-    private LinkedList<Turn> turns;
-    private HashMap<String, Integer> current_scores;
-    private int winningPlayerIndex;
-    private int shots_per_turn;
+    private boolean doubleIn;
+    private boolean doubleOut;
 
-    X01_game() {
-        this(new String[]{"Player One"}, 501, false, false, "local_game");
-    }
+    X01_game() {  }
 
-    X01_game(String[] players, int scoreGoal, boolean double_in, boolean double_out, String game_id) {
+    @Override
+    public void setInitialScores() {
+        Set<String> players = getPlayersReady().getPlayersReady().keySet();
+        HashMap<String, Integer> initialScores = new HashMap<>(players.size());
 
-        initializeFields(players, scoreGoal, double_in, double_out, game_id);
-
-        initializeGameInDatabase(game_id);
-
-        initializeDatabaseListeners(game_id);
-    }
-
-    private void initializeFields
-            (String[] players, int score_goal, boolean double_in, boolean double_out, String game_id) {
-
-        this.players = new String[players.length];
-        start_time = Calendar.getInstance().getTime();
-        System.arraycopy(players, 0, this.players, 0, players.length);
-
-        this.playerTurnIndex = 0;
-        this.scoreGoal = score_goal;
-        this.double_in = double_in;
-        this.double_out = double_out;
-        this.winningPlayerIndex = -1;
-        this.shots_per_turn = 3;
-        this.game_id = game_id;
-
-        current_scores = new HashMap<String, Integer>();
-        turns = new LinkedList<Turn>();
-
-        game_ref = FirebaseDatabase.getInstance().getReference().child("game").child(game_id);
-    }
-    private void initializeGameInDatabase(String game_id) {
-
-        String game_name = "X01";
-        DatabaseReference game_ref = FirebaseDatabase.getInstance().getReference();
-        game_ref = game_ref.child("game").child(game_id).child("game");
-
-        game_ref.child("start_time").setValue(start_time);
-        game_ref.child("game_type").setValue(game_name);
-        game_ref.child("players").push().setValue("Ziltoid");
-
-    }
-    private void initializeDatabaseListeners(String game_id) {
-
-        DatabaseReference mostRecentTurnRef = game_ref.child("most_recent_turn");
-        ValueEventListener mostRecentTurnListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                ArrayList<Integer> database_values = (ArrayList<Integer>) dataSnapshot.child("values").getValue();
-                ArrayList<Turn.Modifier> database_mods = (ArrayList<Turn.Modifier>) dataSnapshot.child("mods").getValue();
-                //Turn t = makeTurn(database_values,database_mods);
-                //updateCurrentTurn(t);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) { }
-        };
-        mostRecentTurnRef.addValueEventListener(mostRecentTurnListener);
-
-        DatabaseReference currentScoresRef = game_ref.child("current_scores");
-        ValueEventListener currentScoresListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // TODO: No way in hell this works
-                Turn turn = (Turn) dataSnapshot.getValue();
-                updateCurrentScoresFromDatabase(turn);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-        currentScoresRef.addValueEventListener(currentScoresListener);
-
-        DatabaseReference winnerRef = game_ref.child("game").child("winner");
-        ValueEventListener winnerListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // TODO: implement
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-        winnerRef.addValueEventListener(winnerListener);
-    }
-
-    private int getPlayerTurnIndex() {
-        return playerTurnIndex;
-    }
-
-    private void setPlayerTurnIndex(int player_turn_index) {
-        this.playerTurnIndex = player_turn_index % players.length;
-    }
-
-    boolean addTurn(ArrayList<Integer> shot_values, ArrayList<Turn.Modifier> mod_values, String player_id) {
-
-        Turn turn = makeTurn(shot_values, mod_values, player_id);
-
-        // and update the database, this will also trigger the local listener to update local data
-        addTurnToDatabase(turn);
-
-        /*
-        if (winCondition()) {
-            updateWinnerDatabase();
+        // Everybody starts with a scoreGoal to hit
+        for (String player : players) {
+            initialScores.put(player, scoreGoal);
         }
-        */
-        return true;
+        setCurrentScores(initialScores);
     }
 
-    private boolean winCondition() {
-        return current_scores.get(getCurrentPlayer()) == 0;
+    @Override
+    public Integer calculateNewScore(Turn turn) {
+        int oldScore = getCurrentScores().get(turn.getPlayerId());
+        return oldScore - turn.getPointTotal();
     }
 
-    private void updateWinnerDatabase() {
-        DatabaseReference winnerRef = game_ref.child("game").child("winner");
-        winnerRef.setValue(getCurrentPlayer());
-    }
-
-    public String getCurrentPlayer() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        return user.getDisplayName();
-    }
-
-    private Turn makeTurn(ArrayList<Integer> shot_values, ArrayList<Turn.Modifier> mod_values, String player_id) {
-
-        Turn turn = new Turn(player_id);
-
-        int point_total = 0;
-        for (int i = 0; i < 3; i++) {
-            int shotValue = shot_values.get(i);
-            Turn.Modifier modValue = mod_values.get(i);
-            turn.addShot(shotValue, modValue);
-            switch (mod_values.get(i)) {
-                case Double:
-                    shot_values.set(i, shot_values.get(i) * 2);
-                    break;
-                case Triple:
-                    shot_values.set(i, shot_values.get(i) * 3);
-                    break;
+    @Override
+    String winConditionMet() {
+        // The last shot may need to be a double
+        if (doubleOut && !getMostRecentTurn().lastShotIsDouble()) {
+            return null;
+        }
+        // when a player has a score of exactly 0, they win
+        if (getCurrentScores().containsValue(0)) {
+            for (String playerId : getCurrentScores().keySet()) {
+                if (getCurrentScores().get(playerId) == 0) {
+                    setEndTime(Calendar.getInstance().getTime());
+                    return playerId;
+                }
             }
-            point_total += shot_values.get(i);
+        }
+        return null;
+    }
+
+    X01_game(int scoreGoal, boolean double_in, boolean double_out, String gameId, PlayersReady playersReady) {
+
+        // 3 shots in a 01 game
+        super(3, gameId, playersReady);
+
+        this.scoreGoal = scoreGoal;
+        this.doubleIn = double_in;
+        this.doubleOut = double_out;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeInt(this.scoreGoal);
+        dest.writeByte(this.doubleIn ? (byte) 1 : (byte) 0);
+        dest.writeByte(this.doubleOut ? (byte) 1 : (byte) 0);
+    }
+
+    protected X01_game(Parcel in) {
+        super(in);
+        this.scoreGoal = in.readInt();
+        this.doubleIn = in.readByte() != 0;
+        this.doubleOut = in.readByte() != 0;
+    }
+
+    public static final Creator<X01_game> CREATOR = new Creator<X01_game>() {
+        @Override
+        public X01_game createFromParcel(Parcel source) {
+            return new X01_game(source);
         }
 
-        turn.setPointTotal(point_total);
-        return turn;
-    }
-
-    private void updateCurrentScoresFromDatabase(Turn turn) {
-
-        //current_scores.put(turn.getPlayerId(), turn.getPointTotal());
-    }
-
-    private void addTurnToDatabase(Turn turn) {
-        DatabaseReference turn_reference = game_ref.child("turns");
-        turn_reference.push().setValue(turn);
-
-        DatabaseReference most_recent_turn_ref = game_ref.child("most_recent_turn");
-        most_recent_turn_ref.setValue(turn);
-    }
-
-    int getCurrentScore(String player) {
-        return current_scores.get(player);
-    }
-
-    public int getShotsPerTurn() {
-        return shots_per_turn;
-    }
-
-    public int getFirstPlayerScore() {
-        return current_scores.get(players[0]);
-    }
-
-    public Turn getLatestTurn() {
-        return turns == null || turns.size() == 0 ? null : turns.getLast();
-    }
+        @Override
+        public X01_game[] newArray(int size) {
+            return new X01_game[size];
+        }
+    };
 }
